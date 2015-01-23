16298dcf461653c318037de6ba1177d654bf4fcb
Sumit Semwal <sumit.semwal@linaro.org>
2015-01-23 12:53:43 +0530

    [Backport] dma-buf: cleanup dma_buf_export() to make it easily extensible
    At present, dma_buf_export() takes a series of parameters, which
    makes it difficult to add any new parameters for exporters, if required.
    
    Make it simpler by moving all these parameters into a struct, and pass
    the struct * as parameter to dma_buf_export().
    
    While at it, unite dma_buf_export_named() with dma_buf_export(), and
    change all callers accordingly.
    
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@canonical.com>
    Reviewed-by: Daniel Thompson <daniel.thompson@linaro.org>
    Acked-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>
    Acked-by: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>
    
    This is only a partial commit from:
    1. https://github.com/aosp-mirror/kernel_common/commit/d8fbe341beb617ebb22b98fb893e4aa32ae2d864#diff-44c558f3b3395009a1729a9d5da1585e
    2. https://github.com/aosp-mirror/kernel_common/commit/72449cb47b0104c32ff8fb9380ade9113375d8d1
    
    The rest are merged in commit b6408f99ebc6b55388b5c4e8178facd991713110.

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1418,6 +1418,7 @@ static struct dma_buf *__ion_share_dma_buf(struct ion_client *client,
 	struct ion_buffer *buffer;
 	struct dma_buf *dmabuf;
 	bool valid_handle;
+	DEFINE_DMA_BUF_EXPORT_INFO(exp_info);
 
 	if (lock_client)
 		mutex_lock(&client->lock);
@@ -1433,8 +1434,12 @@ static struct dma_buf *__ion_share_dma_buf(struct ion_client *client,
 	if (lock_client)
 		mutex_unlock(&client->lock);
 
-	dmabuf = dma_buf_export(buffer, &dma_buf_ops, buffer->size, O_RDWR,
-				NULL);
+	exp_info.ops = &dma_buf_ops;
+	exp_info.size = buffer->size;
+	exp_info.flags = O_RDWR;
+	exp_info.priv = buffer;
+
+	dmabuf = dma_buf_export(&exp_info);
 	if (IS_ERR(dmabuf)) {
 		ion_buffer_put(buffer);
 		return dmabuf;
