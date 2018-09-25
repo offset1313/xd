ce1ba0cf3ebb037eade8806ddfe7ade90e6ad346
Albert I <krascgq@outlook.co.id>
2018-09-25 19:47:09 +0800

    Revert "kernel/lib: add additional debug capabilites for data corruption"
    This reverts commit 5a249fd097a2663371031e3a80d6d1dbe5c638cd.
    
     Conflicts:
    	kernel/locking/spinlock_debug.c
    	lib/Kconfig.debug
    
    Change-Id: Ifb60eb4f9c602dc4bba597cfec32cc8a3a78c184
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- lib/list_debug.c
+++ lib/list_debug.c
@@ -11,7 +11,6 @@
 #include <linux/kernel.h>
 #include <linux/rculist.h>
 
-
 /*
  * Check that the data structures for the list manipulations are reasonably
  * valid. Failures here indicate memory corruption (and possibly an exploit
@@ -21,6 +20,7 @@
 bool __list_add_valid(struct list_head *new, struct list_head *prev,
 		      struct list_head *next)
 {
+<<<<<<< HEAD
 	if (CHECK_DATA_CORRUPTION(next->prev != prev,
 			"list_add corruption. next->prev should be prev (%p), but was %p. (next=%p).\n",
 			prev, next->prev, next) ||
@@ -33,6 +33,23 @@ bool __list_add_valid(struct list_head *new, struct list_head *prev,
 		return false;
 
 	return true;
+=======
+	WARN(next->prev != prev,
+		"list_add corruption. next->prev should be "
+		"prev (%p), but was %p. (next=%p).\n",
+		prev, next->prev, next);
+	WARN(prev->next != next,
+		"list_add corruption. prev->next should be "
+		"next (%p), but was %p. (prev=%p).\n",
+		next, prev->next, prev);
+	WARN(new == prev || new == next,
+	     "list_add double add: new=%p, prev=%p, next=%p.\n",
+	     new, prev, next);
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
+>>>>>>> a01876d6c525... Revert "kernel/lib: add additional debug capabilites for data corruption"
 }
 EXPORT_SYMBOL(__list_add_valid);
 
@@ -43,6 +60,7 @@ bool __list_del_entry_valid(struct list_head *entry)
 	prev = entry->prev;
 	next = entry->next;
 
+<<<<<<< HEAD
 	if (CHECK_DATA_CORRUPTION(next == LIST_POISON1,
 			"list_del corruption, %p->next is LIST_POISON1 (%p)\n",
 			entry, LIST_POISON1) ||
@@ -58,6 +76,39 @@ bool __list_del_entry_valid(struct list_head *entry)
 		return false;
 
 	return true;
+=======
+	if (WARN(next == LIST_POISON1,
+		"list_del corruption, %p->next is LIST_POISON1 (%p)\n",
+		entry, LIST_POISON1) ||
+	    WARN(prev == LIST_POISON2,
+		"list_del corruption, %p->prev is LIST_POISON2 (%p)\n",
+		entry, LIST_POISON2) ||
+	    WARN(prev->next != entry,
+		"list_del corruption. prev->next should be %p, "
+		"but was %p\n", entry, prev->next) ||
+	    WARN(next->prev != entry,
+		"list_del corruption. next->prev should be %p, "
+		"but was %p\n", entry, next->prev))
+		return;
+
+	__list_del(prev, next);
+}
+EXPORT_SYMBOL(__list_del_entry);
+
+/**
+ * list_del - deletes entry from list.
+ * @entry: the element to delete from the list.
+ * Note: list_empty on entry does not return true after this, the entry is
+ * in an undefined state.
+ */
+void list_del(struct list_head *entry)
+{
+	__list_del_entry(entry);
+	entry->next = LIST_POISON1;
+	entry->prev = LIST_POISON2;
+}
+EXPORT_SYMBOL(list_del);
+>>>>>>> a01876d6c525... Revert "kernel/lib: add additional debug capabilites for data corruption"
 
 }
 EXPORT_SYMBOL(__list_del_entry_valid);
